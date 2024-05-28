26ea242ecb01301380d35d302be007f2b4359029
Khrystyna <k-r-i-s-t-i-n-a@i.ua>
2024-05-28 18:39:17 +0300

    Added mantra-add-form

--- mantra-ui/src/App.tsx
+++ mantra-ui/src/App.tsx
@@ -3,9 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
+    <div className="AppContainer">
+      <form className='add-mantra-form'>
+        <input required
+        name='mantra-name'
+        type='text'
+        placeholder='Mantra name'
+        />
+        <input required
+        name='mantra-cat'
+        type='text'
+        placeholder='Mantra category'
+        />
+        <input required
+        name='mantra-content'
+        type='textarea'
+        placeholder='Mantra'
+        />
+        <button type='submit' className='add-btn'>Add mantra</button>
+      </form>
 
     </div>
+    </React.Fragment>
   );
 }
 
